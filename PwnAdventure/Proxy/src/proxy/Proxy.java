019b487f7bbe2a0c95704b71e4750d30e681335c
Alaa <b178y73@gmail.com>
2020-02-16 00:31:47 +0000

    first commit

---
+++ PwnAdventure/Proxy/src/proxy/Proxy.java
@@ -0,0 +1,19 @@
+
+package proxy;
+/**
+ *
+ * @author alaa
+ *  TODO : 
+ *          1 - Game2Proxy      ===> using ServerSocket (This class implements server sockets,
+ *                                                       our proxy is acting as a Server for the Game Client)
+ *          2 - Proxy2Server    ===> using Socket (This class implements client sockets, 
+ *                                              our proxy here is acting as a Client for the Game Server)
+ *          3 - Parsing Packets ===> NO IDEA YET!
+ */
+public class Proxy 
+{
+    public static void main(String[] args) 
+    {
+      
+    } // end main
+} // end class 
