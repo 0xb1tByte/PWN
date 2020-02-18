18d0e3d52f791d0ba65c9cfac962a060604a7a89
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-02-18 19:39:00 +0000

    pushing last update 4 parsing (not completed yet)
    still working on changePlayerLocation()

--- PwnAdventure/Proxy/src/proxy/Proxy.java
+++ PwnAdventure/Proxy/src/proxy/Proxy.java
@@ -8,10 +8,10 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
- /* NEW TODO : 
- * 1 - Proxy ==> This class intercepts the traffic between the "Game Server" & "Client", 
-                    and Forward the packets between them using threads =====================> DONE
- * 2 - Parsing Packets ===> NO IDEA YET!
+/**
+ * @author alaa NEW TODO : 1 - Proxy ==> This class intercepts the traffic
+ * between the "Game Server" & "Client", and Forward the packets between them
+ * using threads 2 - Parsing Packets ===> NO IDEA YET!
  */
 public class Proxy {
 
@@ -21,9 +21,9 @@ public class Proxy {
 
     // Client Variables
     private Socket clientSocket; // Client socket
-    private String clientAddress; // to store the Client address
+    private String clientAddress; // to store the client address
     private InputStream fromClient; // Data sent by Client to the Proxy
-    private OutputStream toClient; // Data sent by the Proxy to the Client (the data that we are forwarding from the Game Server)
+    private OutputStream toClient; // Data sent by the Proxy to the client (the data that we are forwarding from the Game Server)
 
     // Game Server Variables
     private Socket gameServerSocket; // Game Server socket
@@ -40,7 +40,7 @@ public class Proxy {
         // 1 - Create a proxy socket and bind it to a specific port number
         // we set the port for the our Proxy 
         this.portNumber = port;
-        // we set the same port for the Game Server Socket
+         // we set the same port for the Game Server Socket
         this.gameServerPort = port;
         proxySocket = new ServerSocket(portNumber);
         // FOR DEBUGGING : 
@@ -59,7 +59,7 @@ public class Proxy {
                 this.clientSocket = this.proxySocket.accept();
                 // getting the client IP 
                 this.clientAddress = this.clientSocket.getInetAddress().getHostAddress();
-                System.out.println("[+] New connection from " + clientAddress); 
+                System.out.println("[+] New connection from " + clientAddress); // PASSED
 
                 // 5 - Read data from the client via an InputStream obtained from the client socket. ==> will be used in the Thread
                 this.fromClient = clientSocket.getInputStream();
@@ -93,6 +93,8 @@ public class Proxy {
                         int bytes_read;
                         try {
                             // while the Client is sending streams , forward them to the Game Server 
+                            // read() : it returns the total number of bytes read into the buffer, 
+                                        // or -1 if there is no more data because the end of the stream has been reached.
                             while ((bytes_read = fromClient.read(request)) != -1) {
                                 toGameServer.write(request, 0, bytes_read);
                                 System.out.println("[+] Client is sending " + bytes_read + " Bytes to the Game Server:\n" + printHex(request));
@@ -149,12 +151,14 @@ public class Proxy {
     } // end listen 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        
+        checkingBytesValues();
       
         int portNumber = 3333; // set the port number to the default port for the Game Server (This port is for Authenticating the Player)
         if (args.length > 0) {
             try {
                 portNumber = Integer.parseInt(args[0]);
-               // create a proxy instance 
+                 // create a proxy instance 
                 Proxy PWNProxy = new Proxy(portNumber);
             } catch (NumberFormatException e) {
                 System.err.println("[!] Argument" + args[0] + " must be an integer.");
@@ -165,7 +169,9 @@ public class Proxy {
         {
          System.out.println("[!] Please enter the port of the Game Server to start the Proxy");
          System.out.println("[~] Usage: java proxy portNumber");
-        } // end if     
+        } // end if
+        // create a proxy instance 
+       
     } // end main
 
     private static String printHex(byte[] bytes) {
@@ -176,5 +182,93 @@ public class Proxy {
         }
         return sb.toString();
 
+    } // end printHex 
+    
+     private static void checkingBytesValues()
+    {
+         // FOR DEBUGGING : 
+        String hex  = "6d762fb254c713be46c7004b5c44000020ae00000000007f0000000037633638666633336362";
+        byte [] test = hexToByteArray(hex);
+        //  printing the bytes values : 
+        for (int i = 0; i < test.length; i++) {
+         System.out.println("Byte at position "+ i + " is : "); 
+         System.out.println(Integer.toString((test[i] & 0xff) + 0x100, 16).substring(1));
+        }       
+    
+    } // end findLocationPacket
+     
+         /* private static void findLocationPacket (bytes [] locationPacket)
+        {
+
+        //  printing the bytes values : 
+        for (int i = 0; i < test.length; i++) 
+        {
+        
+        }  // end for      
+        } // end findLocationPacket */
+     
+    private static byte [] changePlayerLocation (byte [] locationPacket)
+    {
+    //locationPacket
+        byte [] newPostions = null;
+        
+        for (int i = 0 ; i < locationPacket.length ; i++) 
+        {
+           // changing X , X is stored in bytes[2-5]
+            if (i == 2)
+            {} // end if
+            
+            if (i == 3)
+            {} // end if
+            
+            if (i == 4)
+            {} // end if
+            
+            if (i == 5)
+            {} // end if
+            
+           // changing Y , Y is from bytes[6-9]
+            if (i == 6)
+            {} // end if
+            
+            if (i == 7)
+            {} // end if
+            
+            if (i == 8)
+            {} // end if
+            
+            if (i == 9)
+            {} // end if 
+            
+           // changing Z , Z is from bytes[10-13] 
+            if (i == 10)
+            {} // end if
+            
+            if (i == 11)
+            {} // end if
+            
+            if (i == 12)
+            {} // end if
+            
+            if (i == 13)
+            {} // end if 
+            
+            newPostions [i] = locationPacket[i];
+            
+        } // end for 
+        
+        return newPostions;
+    } // end changeLocation
+    
+    
+    private static byte[] hexToByteArray(String s) {
+    int len = s.length();
+    byte[] data = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                             + Character.digit(s.charAt(i+1), 16));
     }
+    return data;
+}
+    
 } // end Proxy class 
